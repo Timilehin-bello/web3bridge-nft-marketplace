628ce9b57e7f741961724c5836e096089b7cdfa7
Timilehin-bello <emmatimmybello@gmail.com>
2024-02-26 17:40:28 +0100

    feat: complete implementation

---
+++ config/config.ts
@@ -0,0 +1,29 @@
+export const config: any = {
+  "31337": {
+    NFTMarketplace: {
+      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    },
+    explorerURL: "#",
+  },
+
+  "1337": {
+    NFTMarketplace: {
+      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    },
+    explorerURL: "#",
+  },
+
+  "80001": {
+    NFTMarketplace: {
+      address: "0xEc888ea9Fd9ea5D5c666E9c33D71D38AD7B29eE5",
+    },
+    explorerURL: "https://mumbai.polygonscan.com/",
+  },
+
+  "5": {
+    NFTMarketplace: {
+      address: "0xAaC02B000B49c1d23b97a442135969eB0F4Da108",
+    },
+    explorerURL: "https://goerli.etherscan.io/",
+  },
+};
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
+  const nftMarketplace = await NFTMarketplace.deploy();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`Deployed contract Address ${nftMarketplace.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
