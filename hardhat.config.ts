c1326b3459346ec9c9fa66de8ad7afdbea658f74
Timilehin-bello <emmatimmybello@gmail.com>
2024-02-26 17:46:50 +0100

    feat: complete implementation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
